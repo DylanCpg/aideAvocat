00a1b088306b92168685e1de9def9b583d861c66
Dylan Campergue <campergue.dylan@gmail.com>
2020-08-19 20:28:07 +0200

    rectifications d'erreurs

--- aideAvocat/src/aideavocat/AideAvocat.java
+++ aideAvocat/src/aideavocat/AideAvocat.java
@@ -12,6 +12,7 @@ package aideavocat;
 
 import java.util.Scanner; 
 import java.util.*;
+import jdk.nashorn.internal.ir.RuntimeNode;
 
 public class AideAvocat {
 
@@ -22,6 +23,11 @@ public class AideAvocat {
       
        int cpt=0;
        int cptModif=0;
+       int test=0;
+       Float rem2=0F;
+       Float rem=0F;
+       
+       
        Scanner sc = new Scanner(System.in);
        
        System.out.println("Pour ajouter un dossier entrez a,pour modifier un dossier tapez s, pour quitter tapez Q");
@@ -32,9 +38,10 @@ public class AideAvocat {
        
        while(!"Q".equals(reponse) && ! "q".equals(reponse)){
            if(cpt>0){
+             System.out.println("\nPour ajouter un dossier entrez a,pour modifier un dossier tapez s, pour quitter tapez Q");
              reponse = sc.nextLine();
-             System.out.println("Pour ajouter un dossier entrez a,pour modifier un dossier tapez s, pour quitter tapez Q");
              reponse = sc.nextLine();
+          
              choice=reponse;
            }
       
@@ -50,24 +57,33 @@ public class AideAvocat {
 
                          System.out.println("Veuillez saisir le descriptif du dossier svp :");
                          String desc = sc.nextLine();
-
-                         System.out.println("Veuillez saisir la rémunération du dossier en euro svp :");
-                         Float rem = sc.nextFloat();
-
+                        
+                            try{
+                           
+                               System.out.println("Veuillez saisir la rémunération du dossier en euro svp :");
+                               rem = sc.nextFloat();
+                              
+                            }catch(Exception e){
+                                System.out.println("valeur non valide remunération mise à 0€");    
+                            }
+                           
+                         
+                        
+                         
                          Informations = new informations(nomAvocat,nomClient,prenomClient,desc,rem);
 
-                         System.out.println("nom de l'avocate sur le dossier: " +Informations.getNameAvocat()+"\nnom du client: "+Informations.getNameClient()+"\nprénom du client: "+Informations.getPrenomClient()+"\ndescriptif de l'affaire: "+Informations.getDesc()+"\nrémunération: "+Informations.getRemuneration()+"€");
+                         System.out.println("\nnom de l'avocat sur le dossier: " +Informations.getNameAvocat()+"\nnom du client: "+Informations.getNameClient()+"\nprénom du client: "+Informations.getPrenomClient()+"\ndescriptif de l'affaire: "+Informations.getDesc()+"\nrémunération: "+Informations.getRemuneration()+"€");
                          break;
 
                  case "s":
                        
-                            System.out.println("Entrez le champs à modifier\n1:nom de l'avocat,\n2:nom du Client,\n3:prenom du client,\n4:descriptif du dossier,\n5:la rémunération,\n6:quitter le mode édition");
+                            System.out.println("\nEntrez le champs à modifier\n1:nom de l'avocat,\n2:nom du Client,\n3:prenom du client,\n4:descriptif du dossier,\n5:la rémunération,\n6:quitter le mode édition");
                             int reponseModif = sc.nextInt();
                             int choiceModif=reponseModif;
+                            cptModif=0;
                             while((choiceModif<=6 && choiceModif>=1)){
                                 if(cptModif>0){
-                                 reponseModif = sc.nextInt();
-                                 System.out.println("Entrez le champs à modifier\n1: nom de l'avocat,\n2:nom du Client,\n3:prenom du client,\n4:descriptif du dossier,\n5:la rémunération\n6:quitter le mode édition");
+                                 System.out.println("\nEntrez le champs à modifier\n1: nom de l'avocat,\n2:nom du Client,\n3:prenom du client,\n4:descriptif du dossier,\n5:la rémunération\n6:quitter le mode édition");
                                  reponseModif = sc.nextInt();
                                  choiceModif=reponseModif;
                                 }
@@ -78,7 +94,7 @@ public class AideAvocat {
                                         System.out.println("Veuillez saisir le nom de l'avocat svp :");
                                         String nomAvocat2 = sc.nextLine();
                                         Informations.setNameAvocat(nomAvocat2);
-                                        System.out.println("nom de l'avocate sur le dossier: " +Informations.getNameAvocat()+"\nnom du client: "+Informations.getNameClient()+"\nprénom du client: "+Informations.getPrenomClient()+"\ndescriptif de l'affaire: "+Informations.getDesc()+"\nrémunération: "+Informations.getRemuneration()+"€");                
+                                        System.out.println("\nnom de l'avocate sur le dossier: " +Informations.getNameAvocat()+"\nnom du client: "+Informations.getNameClient()+"\nprénom du client: "+Informations.getPrenomClient()+"\ndescriptif de l'affaire: "+Informations.getDesc()+"\nrémunération: "+Informations.getRemuneration()+"€");                
                                         cptModif++;
                                         break;
 
@@ -87,7 +103,7 @@ public class AideAvocat {
                                         System.out.println("Veuillez saisir le nom du client svp :");
                                         String nomClient2 = sc.nextLine();
                                         Informations.setNameClient(nomClient2);
-                                        System.out.println("nom de l'avocate sur le dossier: " +Informations.getNameAvocat()+"\nnom du client: "+Informations.getNameClient()+"\nprénom du client: "+Informations.getPrenomClient()+"\ndescriptif de l'affaire: "+Informations.getDesc()+"\nrémunération: "+Informations.getRemuneration()+"€");                
+                                        System.out.println("\nnom de l'avocate sur le dossier: " +Informations.getNameAvocat()+"\nnom du client: "+Informations.getNameClient()+"\nprénom du client: "+Informations.getPrenomClient()+"\ndescriptif de l'affaire: "+Informations.getDesc()+"\nrémunération: "+Informations.getRemuneration()+"€");                
                                         cptModif++;
                                         break;
 
@@ -96,7 +112,7 @@ public class AideAvocat {
                                         System.out.println("Veuillez saisir le prenom du client svp :");
                                         String prenomClient2 = sc.nextLine();
                                         Informations.setPrenom(prenomClient2);
-                                        System.out.println("nom de l'avocate sur le dossier: " +Informations.getNameAvocat()+"\nnom du client: "+Informations.getNameClient()+"\nprénom du client: "+Informations.getPrenomClient()+"\ndescriptif de l'affaire: "+Informations.getDesc()+"\nrémunération: "+Informations.getRemuneration()+"€");                
+                                        System.out.println("\nnom de l'avocate sur le dossier: " +Informations.getNameAvocat()+"\nnom du client: "+Informations.getNameClient()+"\nprénom du client: "+Informations.getPrenomClient()+"\ndescriptif de l'affaire: "+Informations.getDesc()+"\nrémunération: "+Informations.getRemuneration()+"€");                
                                         cptModif++;
                                         break;
 
@@ -105,37 +121,44 @@ public class AideAvocat {
                                         System.out.println("Veuillez saisir le descriptif du dossier svp :");
                                         String desc2 = sc.nextLine();
                                         Informations.setDesc(desc2);
-                                        System.out.println("nom de l'avocate sur le dossier: " +Informations.getNameAvocat()+"\nnom du client: "+Informations.getNameClient()+"\nprénom du client: "+Informations.getPrenomClient()+"\ndescriptif de l'affaire: "+Informations.getDesc()+"\nrémunération: "+Informations.getRemuneration()+"€");                
+                                        System.out.println("\nnom de l'avocate sur le dossier: " +Informations.getNameAvocat()+"\nnom du client: "+Informations.getNameClient()+"\nprénom du client: "+Informations.getPrenomClient()+"\ndescriptif de l'affaire: "+Informations.getDesc()+"\nrémunération: "+Informations.getRemuneration()+"€");                
                                         cptModif++;
                                         break;
 
                                     case 5 :
                                         String remu = sc.nextLine();
                                         System.out.println("Veuillez saisir la rémunération du dossier en euro svp :");
-                                        Float rem2 = sc.nextFloat();
-                                        Informations.setRem(rem2);
-                                        System.out.println("nom de l'avocate sur le dossier: " +Informations.getNameAvocat()+"\nnom du client: "+Informations.getNameClient()+"\nprénom du client: "+Informations.getPrenomClient()+"\ndescriptif de l'affaire: "+Informations.getDesc()+"\nrémunération: "+Informations.getRemuneration()+"€");                
-                                        cptModif++;
+                                       
+                                        try{
+                                            rem2 = sc.nextFloat();
+                                            Informations.setRem(rem2);
+                                            System.out.println("\nnom de l'avocate sur le dossier: " +Informations.getNameAvocat()+"\nnom du client: "+Informations.getNameClient()+"\nprénom du client: "+Informations.getPrenomClient()+"\ndescriptif de l'affaire: "+Informations.getDesc()+"\nrémunération: "+Informations.getRemuneration()+"€");                
+                                            cptModif++;
+                                            
+                                        }catch(Exception e){
+                                            System.out.println("pas la bonne valeur");
+                                        }
                                         break;
                                     
-                                    case 6:System.out.println("quitter mode edition");
+                                    case 6:System.out.println("quitter mode edition\n");
                                             cptModif++;
                                             choiceModif=8;
                                             break;
                                         
-                                    default: System.out.println("veuillez saisir un argument valide svp 1");
+                                    default: System.out.println("veuillez saisir un argument valide svp");
                                              break;
                                 }
                                
                             } 
                            break;
                  case "q": 
-                        System.out.println("ici");
                         System.exit(0);
-                 
-                 case "Q": System.exit(0);
-                 
-                 default: System.out.println("veuillez saisir un argument valide svp 2");
+                      
+                        
+                 case "Q": 
+                     System.exit(0);
+                     
+                 default: System.out.println("veuillez saisir un argument valide svp");
                           break;
                 
            } 
